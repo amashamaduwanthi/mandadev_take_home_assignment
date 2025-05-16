d902ac280edb09e96cebbc6932680aa8f3cf12ab
amashamaduwanthi <amashamaduwanthi123@gmail.com>
2025-05-16 19:41:40 +0530

    add tailwind css and User.ts create

---
+++ take-home-assignment-main/assignment_frontend/src/App.tsx
@@ -0,0 +1,28 @@
+import {Provider} from "react-redux";
+import {createBrowserRouter, RouterProvider} from 'react-router';
+import './App.css'
+import AuthLayout from "./components/AuthLayOut.tsx";
+import {SignIn} from "./pages/SignIn.tsx";
+import {SignUp} from "./pages/SignUp.tsx";
+import {store} from "./store/store.ts";
+function App() {
+    const routes=createBrowserRouter([
+        {
+            path: "",
+            element: <AuthLayout />,
+            children: [
+                { path: "/signin", element: <SignIn /> },
+                { path: "", element: <SignUp /> },
+            ],
+        },
+    ])
+    return (
+      <>
+          <Provider store={store}>
+              <RouterProvider router={routes}/>
+          </Provider>
+      </>
+  )
+}
+
+export default App
